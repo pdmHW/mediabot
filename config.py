a9c369c547ac47b1cb3c102db80dbab8596471ee
pdmHW <pdalermunisovich@gmail.com>
2026-04-30 18:09:13 +0500

    Add configuration settings for bot and secrets

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "" #BOT_TOKEN
+OWNER_ID = 12345678 #Telegram_ID
+DB_PATH = "DIRECTION" 
+
+
+# ── Easter Egg secrets ──
+SECRET_BYPASS_CODE = "PASS"   # Skip Channels code
+SECRET_COMMAND = "neo"                                # Matrix Mode code
+KONAMI_SEQUENCE = ["a", "b", "c", "d"]
+DEV_MODE_CODE = "IDKMAN"           # For people who found Konami
+
